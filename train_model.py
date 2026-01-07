be90bde4adfe1241da7c9016561345bd98f945f3
Aniruddha awate <anuawate565@gmail.com>
2026-01-07 22:50:34 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import joblib
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from xgboost import XGBRegressor
 import json
